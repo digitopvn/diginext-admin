a68e65cfd2fc475640a1539afd80432fc2e7f49b
digitop-duynguyen <digitop-duynguyen>
2023-01-01 11:11:09 +0700

    feat(first commit): initial commit

---
+++ src/templates/Main.tsx
@@ -0,0 +1,65 @@
+import Link from "next/link";
+import type { ReactNode } from "react";
+
+import { AppConfig } from "@/utils/AppConfig";
+
+type IMainProps = {
+	meta: ReactNode;
+	children: ReactNode;
+};
+
+const Main = (props: IMainProps) => (
+	<div className="w-full px-1 text-gray-700 antialiased">
+		{props.meta}
+
+		<div className="mx-auto max-w-screen-md">
+			<div className="border-b border-gray-300">
+				<div className="pt-16 pb-8">
+					<div className="text-3xl font-bold text-gray-900">{AppConfig.title}</div>
+					<div className="text-xl">{AppConfig.description}</div>
+				</div>
+				<div>
+					<ul className="flex flex-wrap text-xl">
+						<li className="mr-6">
+							<Link href="/" className="border-none text-gray-700 hover:text-gray-900">
+								Home
+							</Link>
+						</li>
+						<li className="mr-6">
+							<Link href="/about/" className="border-none text-gray-700 hover:text-gray-900">
+								About
+							</Link>
+						</li>
+						<li className="mr-6">
+							<a
+								className="border-none text-gray-700 hover:text-gray-900"
+								href="https://bitbucket.org/digitopvn/diginext13/src/master/"
+							>
+								GitRepo
+							</a>
+						</li>
+						<li className="mr-6">
+							<Link href="/blog/" className="border-none text-gray-700 hover:text-gray-900">
+								Blog
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+
+			<div className="content py-5 text-xl">{props.children}</div>
+
+			<div className="border-t border-gray-300 py-8 text-center text-sm">
+				© Copyright {new Date().getFullYear()} {AppConfig.title}. Powered by <a href="https://wearetopgroup.com">TOP GROUP</a>.
+				{/*
+				 * PLEASE READ THIS SECTION
+				 * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
+				 * The link doesn't need to appear on every pages, one link on one page is enough.
+				 * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
+				 */}
+			</div>
+		</div>
+	</div>
+);
+
+export { Main };
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+	site_name: "Diginext13 Starter",
+	title: "Diginext13 Starter Template",
+	description: "Starter code for your Nextjs Boilerplate with Typescript & Tailwind CSS",
+	locale: "en",
+};
