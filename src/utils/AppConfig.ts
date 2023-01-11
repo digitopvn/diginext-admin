8f99be1c92fd15928a55df24f9d2eea4405be85b
duy-nguyen-ngoc <duy-nguyen-ngoc>
2023-01-11 13:17:46 +0700

    style(icons): favicons and meta tags

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,9 +1,9 @@
 // FIXME: Update this configuration file based on your project information
 
 export const AppConfig = {
-	site_name: "Diginext Site",
-	title: "Diginext Site",
-	description: "Admin Site of Diginext",
+	site_name: "Diginext Admin",
+	title: "Diginext Admin",
+	description: "Welcome to an Admin Panel of Diginext.",
 	locale: "en",
 	tableConfig: {
 		defaultPageSize: 20,
