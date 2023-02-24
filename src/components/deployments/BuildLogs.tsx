e9153f5a98eac1fdb2e38507cf4b934ab2381531
digitop-duynguyen <digitop-duynguyen>
2023-02-24 16:17:03 +0700

    fix(build logs): show clickable link when possible

--- src/components/deployments/BuildLogs.tsx
+++ src/components/deployments/BuildLogs.tsx
@@ -5,6 +5,8 @@ import parser from "html-react-parser";
 import { isEmpty } from "lodash";
 import React, { useEffect, useState } from "react";
 import sanitizeHtml from "sanitize-html";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import isURL from "validator/lib/isURL";
 
 import { useBuildLogsApi } from "@/api/api-build";
 import { useRouterQuery } from "@/plugins/useRouterQuery";
@@ -140,11 +142,22 @@ export const BuildLogs = ({ slug }: { slug?: string }) => {
 					.filter((m) => m !== "")
 					.reverse()
 					.map((message, index) => {
-						if (`${message}`.toLowerCase().indexOf("error") > -1)
+						const msg = `${message}`;
+						if (msg.toLowerCase().indexOf("error") > -1)
 							return {
 								dot: <CloseCircleOutlined className="text-red-600" />,
 								children: <span className="text-red-600">{parser(`${message}`)}</span>,
 							};
+
+						if (msg.indexOf("http://") > -1 || msg.indexOf("https://")) {
+							const words = msg.split(" ");
+							const msgWithLink = words
+								.map((m) =>
+									isURL(m, { require_protocol: true }) ? `<a href="${m}" target="_blank" style="color: #008dff">${m}</a>` : m
+								)
+								.join(" ");
+							return { children: parser(msgWithLink) };
+						}
 
 						return { children: parser(`${message}`) };
 					})}
