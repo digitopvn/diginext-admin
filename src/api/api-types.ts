4c509a188c818ebcb5a320ec8032ff08fc2de074
Goon <duy@wearetopgroup.com>
2024-11-15 14:17:24 +0700

    feat(deploy environment): list with filter and cpu,memory

--- src/api/api-types.ts
+++ src/api/api-types.ts
@@ -910,6 +910,11 @@ export interface DiginextEnvironmentVariable {
 }
 
 export interface IDeployEnvironment {
+	env?: string;
+	appSlug?: string;
+	appName?: string;
+	projectSlug?: string;
+	projectName?: string;
 	/**
 	 * Container registry slug
 	 */
@@ -959,7 +964,24 @@ export interface IDeployEnvironment {
 	size?: "none" | "1x" | "2x" | "3x" | "4x" | "5x" | "6x" | "7x" | "8x" | "9x" | "10x";
 	cpu?: string;
 	memory?: string;
+	resources?: {
+		usage?: {
+			cpu?: string;
+			memory?: string;
+		};
+		limits?: {
+			cpu?: string;
+			memory?: string;
+		};
+	};
+
+	/**
+	 * Inherit configuration from the last deployment
+	 */
 	shouldInherit?: boolean;
+	/**
+	 * Redirect to first domain
+	 */
 	redirect?: boolean;
 	/**
 	 * Container's scaling replicas
